6767834ea6c22c8ad364fcfc41c65fb1b46c4496
Morgan Duverney <morgan.duverney@teachstone.com>
2020-12-09 19:13:21 -0500

    Passing in Component Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Kofi"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Mutt"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Pet Adoption in Virginia"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, { name: "Kofi", animal: "Dog", breed: "Mutt"}),
+    React.createElement(Pet, { name: "Bayley", animal: "Dog", breed: "Mutt"}),
+    React.createElement(Pet, { name: "Nico", animal: "Cat", breed: "American Shorthair"}),
   ]);
 };
 
