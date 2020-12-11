ae00fbd9cad69425664039b79f16ea72b3a150dc
Morgan Duverney <morgan.duverney@teachstone.com>
2020-12-11 11:47:42 -0500

    Covert to JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { createElement } from "react";
+import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return createElement("div", {}, [
-    createElement("h1", {}, "Pet Adoption in Virginia"),
-    createElement(Pet, { name: "Kofi", animal: "Dog", breed: "Mutt" }),
-    createElement(Pet, { name: "Bayley", animal: "Dog", breed: "Mutt" }),
-    createElement(Pet, {
-      name: "Nico",
-      animal: "Cat",
-      breed: "American Shorthair",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Pet Adoption in Virginia</h1>
+      <Pet name="Kofi" animal="Dog" breed="Mutt" />
+      <Pet name="Bayley" animal="Dog" breed="Mutt" />
+      <Pet name="Nico" animal="Cat" breed="American Shorthair" />
+    </div>
+  );
 };
 
-render(createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
