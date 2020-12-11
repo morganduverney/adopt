f9a6015ca7375d6dd1f91b6d4b6590c3b61cad0d
Morgan Duverney <morgan.duverney@teachstone.com>
2020-12-11 16:06:25 -0500

    Use hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Pet Adoption in Virginia</h1>
-      <Pet name="Kofi" animal="Dog" breed="Mutt" />
-      <Pet name="Bayley" animal="Dog" breed="Mutt" />
-      <Pet name="Nico" animal="Cat" breed="American Shorthair" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option>all</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>all</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
